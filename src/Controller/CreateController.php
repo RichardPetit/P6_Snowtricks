dceee204752cd26fa099f1ccdccc616a8188c2d9
Richard Petit <richard-petit@live.fr>
2021-08-02 16:39:39 +0200

    replace invoke in createcontroller

--- src/Controller/CreateController.php
+++ src/Controller/CreateController.php
@@ -10,13 +10,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/create", name="trick_create")
+ * @Route("/edit/{id}", name="trick_edit")
+ */
 class CreateController extends AbstractController
 {
-    /**
-     * @Route("/create", name="trick_create")
-     * @Route("/edit/{id}", name="trick_edit")
-     */
-    public function index(Tricks $trick = null,Request $request): Response
+
+    public function __invoke(Tricks $trick = null,Request $request): Response
     {
 
         if (!$trick){
@@ -47,35 +49,35 @@ class CreateController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/trick/new", name="trick_new", methods={"GET","POST"})
-     * @param Request $request
-     * @param Slugify $slugify
-     * @return Response
-     */
-    public function new(Request $request, Slugify $slugify): Response
-    {
-        $trick = new Tricks();
-        $form = $this->createForm(TrickType::class, $trick);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $slug = $slugify->generate($trick->getName());
-            $trick->setSlug($slug);
-            $entityManager->persist($trick);
-            $entityManager->flush();
-
-
-            $this->addFlash('success', 'La figure a été ajoutée');
-            return $this->redirectToRoute('home');
-        }
-
-        return $this->render('create/new.html.twig', [
-            'trick' => $trick,
-            'form' => $form->createView(),
-        ]);
-    }
+//    /**
+//     * @Route("/trick/new", name="trick_new", methods={"GET","POST"})
+//     * @param Request $request
+//     * @param Slugify $slugify
+//     * @return Response
+//     */
+//    public function new(Request $request, Slugify $slugify): Response
+//    {
+//        $trick = new Tricks();
+//        $form = $this->createForm(TrickType::class, $trick);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $entityManager = $this->getDoctrine()->getManager();
+//            $slug = $slugify->generate($trick->getName());
+//            $trick->setSlug($slug);
+//            $entityManager->persist($trick);
+//            $entityManager->flush();
+//
+//
+//            $this->addFlash('success', 'La figure a été ajoutée');
+//            return $this->redirectToRoute('home');
+//        }
+//
+//        return $this->render('create/new.html.twig', [
+//            'trick' => $trick,
+//            'form' => $form->createView(),
+//        ]);
+//    }
 
 
 }

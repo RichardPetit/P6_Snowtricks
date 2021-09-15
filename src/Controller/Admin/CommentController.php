767cca24478761a0198edfeb47cf31342cdad639
Richard Petit <richard-petit@live.fr>
2021-09-15 12:32:58 +0200

    users-liste create and comments-liste upgraded

--- src/Controller/Admin/CommentController.php
+++ src/Controller/Admin/CommentController.php
@@ -2,27 +2,41 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Comment;
 use App\Repository\CommentRepository;
+use App\Repository\TricksRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/admin/comments-list", name="comment")
+ * @Route("/admin/comments-list/{id}", name="comment")
  */
 
 class CommentController extends AbstractController
 {
 
-    public function __invoke(CommentRepository $commentRepository, Request $request): Response
+    public function __invoke(CommentRepository $commentRepository, TricksRepository $tricksRepository, int $id, Request $request, EntityManagerInterface $em): Response
     {
-        $comments = $commentRepository->findAll();
+        $trick = $tricksRepository->find($id);
+        $comments = $this->getComments($commentRepository, $id);
+        if ($trick === null){
+            return $this->redirectToRoute('admin_home');
+        }
+
 
         return $this->render('comment/index.html.twig', [
-            'controller_name' => 'CommentController',
             'comments' => $comments,
+            'trick' => $trick
 
         ]);
     }
+
+    private function getComments(CommentRepository $commentRepository, int $id): ?Comment
+    {
+        return $commentRepository->find($id);
+    }
+
 }
---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/users-list", name="user")
+ */
+class UserController extends AbstractController
+{
+
+    public function __invoke(UserRepository $userRepository, Request $request): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}

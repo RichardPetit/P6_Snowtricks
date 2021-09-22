972908e921669e22566095d67736da20db5105a1
Richard Petit <richard-petit@live.fr>
2021-09-22 14:25:55 +0200

    delete media works

---
+++ src/Controller/Admin/DeleteMediaController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\TrickMedia;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/medias/{id}/delete", name="delete_media", methods={"GET"})
+ * @param TrickMedia $media
+ * @param EntityManagerInterface $em
+ * @return Response
+ */
+class DeleteMediaController extends AbstractController
+{
+
+    public function __invoke(EntityManagerInterface $em, TrickMedia $media): Response
+    {
+        if ($media !==null){
+            $em->remove($media);
+            $em->flush();
+        }
+        return $this->redirectToRoute('admin_home');
+
+    }
+}

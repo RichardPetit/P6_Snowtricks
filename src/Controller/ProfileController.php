dcd089900f18959fed73b4323e83a127e0e68e73
Richard Petit <richard-petit@live.fr>
2021-10-06 16:58:41 +0200

    edit profile started

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +17,42 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ProfileController extends AbstractController
 {
-    public function __invoke(): Response
+    /**
+     * @param User $user
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param $id
+     * @return Response
+     */
+    public function __invoke(Request $request, EntityManagerInterface  $em, User $user = null): Response
+    {
+        if ($user === null){
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->updateProfile($user, $em);
+
+            return $this->redirectToRoute('profile/index.html.twig');
+        }
+
+        return $this->renderUserForm($form, $user);
+    }
+
+    private function renderUserForm( FormInterface $form, User $user): Response
     {
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'formTrick' => $form->createView(),
+            'user' => $user,
         ]);
+    }
+
+    private function updateProfile(User $user, EntityManagerInterface $em)
+    {
+        $em->persist($user);
+        $em->flush();
     }
 }
